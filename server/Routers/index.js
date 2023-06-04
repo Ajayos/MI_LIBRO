a1ea498c8c6c873cf1ddce88010e05456d0909b7
Ajay o s <ajayosakhub@gmail.com>
2023-06-04 13:21:16 +0530

    update

---
+++ server/Routers/index.js
@@ -0,0 +1,40 @@
+/**
+ * Router index file.
+ *
+ * @project : MI LIBRO
+ * @version : 1.0.2
+ * @link : https://github.com/Ajayos/MI_LIBRO
+ * @authors : Ajay, Akarsh, Abinas, Saran, Yasir
+ * @created : 2023-05-17 10:04:13
+ * @modified : 2023-05-17 21:15:30
+ * @editor : Ajayos
+ * @file : index.js
+ * @path : /Routers/index.js
+ *
+ * Description: This file serves as the index file for the Routers folder. It imports and exports the router modules.
+ *
+ * GitHub Repository: https://github.com/Ajayos/MI_LIBRO
+ *
+ * All rights reserved. (C) 2023 Ajayos and co-authors (Akarsh, Abinas, Saran, Yasir)
+ */
+
+// Import dependencies
+const express = require("express");
+const router = express.Router();
+
+// Import individual router modules
+const adminRouter = require("./adminRouter");
+const userRouter = require("./userRouter");
+const booksRouter = require("./bookRouter");
+const likeRouter = require("./likeRouter");
+const commentRouter = require("./commentRouter");
+
+// Set up routes
+router.use("/admins", adminRouter);
+router.use("/users", userRouter);
+router.use("/books", booksRouter);
+router.use("/likes", likeRouter);
+router.use("/comments", commentRouter);
+
+// Export router
+module.exports = router;
---
+++ server/lib/Admin.js
@@ -0,0 +1,127 @@
+/**
+ * Admin Functions.
+ *
+ * @project : MI LIBRO
+ * @version : 1.0.2
+ * @link : https://github.com/Ajayos/MI_LIBRO
+ * @authors : Ajay, Akarsh, Abinas, Saran, Yasir
+ * @created : 2023-05-17 10:04:13
+ * @modified : 2023-06-03 21:15:30
+ * @editor : Ajayos
+ * @file : Admin.js
+ * @path : /lib/Admin.js
+ *
+ * Description: This file define the Admin control for connect to database and authentications
+ *
+ * GitHub Repository: https://github.com/Ajayos/MI_LIBRO
+ *
+ * All rights reserved. (C) 2023 Ajayos and co-authors (Akarsh, Abinas, Saran, Yasir)
+ *
+ */
+
+// Import dependencies
+const { Admin } = require("../Models");
+
+exports.login = async (data) => {
+  try {
+    // Destructure the data object to get the required properties
+    const { email, password } = data;
+
+    // Find the Admin by email
+    const admin = await Admin.findOne({ email });
+
+    // If admin not found, return error
+    if (!admin) {
+      return {
+        status: 404,
+        message: "admin not found",
+        error: true,
+        data: undefined,
+      };
+    }
+
+    // Compare the provided password with the hashed password in the database
+    const isMatch = await bcrypt.compare(password, admin.password);
+
+    // If passwords don't match, return error
+    if (!isMatch) {
+      return {
+        status: 401,
+        error: true,
+        message: "Invalid credentials",
+        data: undefined,
+      };
+    }
+
+    // Generate and return a token for authentication
+    const token = generateAuthToken(admin);
+
+    return {
+      status: 200,
+      error: false,
+      message: undefined,
+      data: { token, admin },
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      error: true,
+      message: "Internal server error",
+      data: undefined,
+    };
+  }
+};
+
+exports.createAccount = async (data) => {
+  try {
+    // Destructure the data object to get the required properties
+    const { email, password, pic, name } = data;
+
+    // Check if the Admin already exists
+    const existingAdmin = await Admin.findOne({ email });
+    if (existingAdmin) {
+      return {
+        status: 409,
+        error: true,
+        message: "Admin already exists",
+        data: undefined,
+      };
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Create the new user account
+    const newAdmin = new Admin({
+      email,
+      password: hashedPassword,
+      name,
+      pic,
+    });
+    await newAdmin.save();
+
+    const admin_ = await Admin.findOne({ email });
+
+    // Generate and return a token for authentication
+    const token = generateAuthToken(admin_);
+
+    return {
+      status: 201,
+      error: false,
+      message: undefined,
+      data: {
+        token,
+        name,
+        email,
+        pic,
+      },
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      error: true,
+      message: "Internal server error",
+      data: undefined,
+    };
+  }
+};
